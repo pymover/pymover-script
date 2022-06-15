afb980fb51818dbe44dbca58e6067a1c3600df6a
Erik <94395935+zokrtes@users.noreply.github.com>
2022-06-15 21:28:14 +0200

    updated framework

--- pymover.py
+++ pymover.py
@@ -15,41 +15,35 @@ def menu():
     print("[1] Run")
     print("[2] Exit\n")
 
-
 menu()
 option =int(input("Enter your option: "))
 
-running = True
+ans = True
+    
+def main_app():
+    file_source = 'E:\MoviesSource/'
+    file_destination = 'E:\Movies/'
+    
+    time.sleep(1)
+    get_source = os.listdir(file_source)
+    get_destination = os.listdir(file_destination)
 
-while option != 2:
+    print("Source: ", get_source)
+    print("Local: ", get_destination)
+
+    for g in get_source:
+        if os.path.exists(file_destination+g):
+            print("Skipped!", g)
+        else:
+            time.sleep(3)
+            shutil.move(file_source + g, file_destination)
+            print('Moved:', g)
+
+while ans:
     if option == 1:
-
-        file_source = 'E:\Download_movies/'
-        file_destination = 'E:\Movies/'
-
-        time.sleep(3)
-        get_source = os.listdir(file_source)
-        get_destination = os.listdir(file_destination)
-
-        print("Source: ", get_source)
-        print("Local: ", get_destination)
-
-
-
-
-        for g in get_source:
-            for x in get_destination:
-                print(x)
-            if g == x:
-                print("Skipped!", g)
-                pass
-            else:
-                time.sleep(3)
-                shutil.move(file_source + g, file_destination)
-                print('Moved:', g)
-
+        main_app()
     elif option == 2:
-        exit
+        exit()
 
     else:
         print("Invalid option.")
